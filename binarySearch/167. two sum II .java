5f3f2dc785607c42574f6fea3225c5bfcdd5b468
Aparnahegde <92423253+Aparnahegde@users.noreply.github.com>
2024-05-07 19:42:23 +0530

    Create 167. two sum II .java

---
+++ binarySearch/167. two sum II .java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int s=0;
+        int e=numbers.length-1;
+        while(s<e){
+            if(numbers[s]+numbers[e]<target){
+                s++;
+            }
+            else if(numbers[s]+numbers[e]>target){
+                e--;
+            }
+            else{
+                return new int[]{s+1,e+1};
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
