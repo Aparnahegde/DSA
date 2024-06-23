ec145314eb5dc2fe95307305229870ee3b815313
Aparnahegde <92423253+Aparnahegde@users.noreply.github.com>
2024-06-23 21:54:20 +0530

    Create 53.max subarray.java

---
+++ arrays/53.max subarray.java
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/maximum-subarray/
+class Solution {                //KADANES ALGORITHMS
+    public int maxSubArray(int[] nums) {
+        int sum=nums[0];
+        int maxSum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(sum>=0){
+                sum+=nums[i];
+            }
+            else{
+                sum=nums[i];
+            }
+            if(maxSum<sum){
+                maxSum=sum;
+            }
+        }
+        return maxSum;
+    }
+}
