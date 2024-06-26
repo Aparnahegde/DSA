b41822e5496a84ae36a722b00bade5e5c3c38566
aparna <aparnaghegde10@gmail.com>
2024-06-26 20:28:06 +0530

    first

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Binary Search using Recursion
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 57, 89};
+        int target = 57;
+        System.out.println(search(arr, target, 0, arr.length - 1));
+    }
+
+    static int search(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int m = s + (e - s) / 2;
+        if (arr[m] == target) {
+            return m;
+        }
+        if (arr[m] < target) {
+            return search(arr, target, m + 1, e);
+        }
+
+        return search(arr, target, s, m - 1);
     }
 }
