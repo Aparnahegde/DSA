0b980de496bebd936d360f98959363f59d834555
Aparnahegde <92423253+Aparnahegde@users.noreply.github.com>
2024-06-28 20:16:31 +0530

    Update duplicate.java

--- arrays/BasicQuestion/duplicate.java
+++ arrays/BasicQuestion/duplicate.java
@@ -1,26 +1,53 @@
-TC--O(n2)  SC--O(n)
+/*O(n) complexity*/
 
 import java.util.*;
-public class duplicate {
+
+public class duplicate{
     public static void main(String[] args) {
-        int[] arr={2,5,2,4,2,3,5,4};
-       List<Integer> a=new ArrayList<>();
+        int[] arr = {2, 5, 2, 4, 2, 3, 5, 4};
+        Set<Integer> duplicates = findDuplicates(arr);
+        System.out.println(duplicates);
+    }
 
-        for(int j=0;j<arr.length;j++){
-            for(int i=1;i<arr.length-j;i++){
-                if(arr[i]<arr[i-1]){
-                    int temp=arr[i];
-                    arr[i]=arr[i-1];
-                    arr[i-1]=temp;
-                }
-            }
+    public static Set<Integer> findDuplicates(int[] arr) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
 
-        }
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]!=arr[i+1]){
-             a.add(arr[i]);
+        for (int num : arr) {
+            if (!set1.add(num)) {
+                duplicates.add(num);
             }
         }
-        System.out.println(a);
+
+        return duplicates;
     }
 }
+
+
+
+// TC--O(n2)  SC--O(n)
+
+// import java.util.*;
+// public class duplicate {
+//     public static void main(String[] args) {
+//         int[] arr={2,5,2,4,2,3,5,4};
+//        List<Integer> a=new ArrayList<>();
+
+//         for(int j=0;j<arr.length;j++){
+//             for(int i=1;i<arr.length-j;i++){
+//                 if(arr[i]<arr[i-1]){
+//                     int temp=arr[i];
+//                     arr[i]=arr[i-1];
+//                     arr[i-1]=temp;
+//                 }
+//             }
+
+//         }
+//         for(int i=0;i<arr.length-1;i++){
+//             if(arr[i]!=arr[i+1]){
+//              a.add(arr[i]);
+//             }
+//         }
+//         System.out.println(a);
+//     }
+// }
