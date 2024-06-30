40fdf34ef06a43e6d6e7b4a5202e00eaf8324b24
Aparnahegde <92423253+Aparnahegde@users.noreply.github.com>
2024-06-30 18:14:09 +0530

    Update and rename number_of_steps.java to 1342.number_of_steps.java

---
+++ Recursion/1342.number_of_steps.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+class Solution {
+    public int numberOfSteps(int num) {
+        return helper(num,0);
+    }
+    int helper(int num,int steps){
+        if(num==0){
+            return steps;
+        }
+      if(num%2==0){
+        return helper(num/2,steps+1);
+      }
+      return helper(num-1,steps+1);// if num is odd then subtracted 1 from num
+    }
+}
