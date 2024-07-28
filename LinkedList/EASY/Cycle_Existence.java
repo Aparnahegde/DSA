a49ece0cf3af0c1a66f1cdc649c84a3b4bd02d72
Aparna Hegde <92423253+Aparnahegde@users.noreply.github.com>
2024-07-28 09:56:03 +0530

    Update Cycle_Existence.java

--- LinkedList/EASY/Cycle_Existence.java
+++ LinkedList/EASY/Cycle_Existence.java
@@ -15,10 +15,10 @@ class Solution {
         while(p2!=null&&p2.next!=null){                      
             p1=p1.next;                                      
             p2=p2.next.next;
-        }
-      if(p1==p2){
+        if(p1==p2){
         return true;
       }
+        }
         return false;
     }
 }
